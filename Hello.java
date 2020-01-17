67e9a86b45ad5d455323472b2d9a12f0ca75e1de
mclaurindd <59941229+mclaurindd@users.noreply.github.com>
2020-01-17 03:14:24 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,23 @@
+/************************************************
+ * Hello World
+ ************************************************
+ * Darriel McLaurin
+ * Jan 15 2020
+ * 255 and 901
+ ***********************************************/
+
+public class Hello {
+
+   public static void main(String[] args) {
+
+     System.out.println("<Darriel McLaurin>");
+     System.out.println("<Hello World>");
+     System.out.println("<Project description>");
+     System.out.println("<Jan 15 2020>");
+     System.out.println("<255 and 901>");
+     System.out.println();
+     }
+ }
+
+
+
